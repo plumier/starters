f9635889888ae0265f7da1638c8dd21a687a12b4
Ketut Sandiarsa <ktutnik@gmail.com>
2021-01-25 17:15:11 +0800

    Add proper security setup and docs

--- rest-api-mongoose/src/api/user/user-entity.ts
+++ rest-api-mongoose/src/api/user/user-entity.ts
@@ -5,19 +5,37 @@ import { authorize, preSave, route, val } from "plumier"
 import { EntityBase } from "../_shared/entity-base"
 
 @route.controller(c => {
+    // POST /users accessible by public
     c.post().authorize("Public")
+
+    // PUT PATCH DELETE GET /users/{id} only accessible by the user itself 
+    // See the user-policy.ts file how ResourceOwner defined
+    c.actions("Delete", "GetOne", "Patch", "Put").authorize("ResourceOwner")
+
+    // GET /users?limit&offset&filter only accessible by Admin
+    c.getMany().authorize("Admin")
 })
 @collection()
 export class User extends EntityBase {
+    // email will only visible by the user itself
+    @authorize.read("ResourceOwner")
     @val.required()
     @val.unique()
     @val.email()
     email: string
 
+    // password will not visible to anyone
     @authorize.writeonly()
     @val.required()
     password: string
 
+    @val.required()
+    name:string
+
+    // role only can be set by Admin
+    @authorize.write("Admin")
+    // role only visible to the user itself or by Admin
+    @authorize.read("ResourceOwner", "Admin")
     @collection.property({ default: "User" })
     role: "User" | "Admin"
 
--- rest-api-typeorm/src/api/user/user-entity.ts
+++ rest-api-typeorm/src/api/user/user-entity.ts
@@ -5,21 +5,40 @@ import bcrypt from "bcryptjs"
 import { EntityBase } from "../_shared/entity-base"
 
 @route.controller(c => {
+    // POST /users accessible by public
     c.post().authorize("Public")
+
+    // PUT PATCH DELETE GET /users/{id} only accessible by the user itself 
+    // See the user-policy.ts file how ResourceOwner defined
+    c.actions("Delete", "GetOne", "Patch", "Put").authorize("ResourceOwner")
+
+    // GET /users?limit&offset&filter only accessible by Admin
+    c.getMany().authorize("Admin")
 })
 @Entity()
 export class User extends EntityBase {
+    // email will only visible by the user itself
+    @authorize.read("ResourceOwner")
     @val.required()
     @val.unique()
     @val.email()
     @Column()
     email: string
 
+    // password will not visible to anyone
     @authorize.writeonly()
     @val.required()
     @Column()
     password: string
 
+    @val.required()
+    @Column()
+    name:string
+
+    // role only can be set by Admin
+    @authorize.write("Admin")
+    // role only visible to the user itself or by Admin
+    @authorize.read("ResourceOwner", "Admin")
     @Column({ default: "User" })
     role: "User" | "Admin"
 

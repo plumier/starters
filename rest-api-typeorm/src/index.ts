0b390c68a4e6aca7a870fed73e33513aeb5f62a6
Ketut Sandiarsa <ktutnik@gmail.com>
2021-03-09 05:36:46 +0800

    Remove excess code

--- rest-api-typeorm/src/index.ts
+++ rest-api-typeorm/src/index.ts
@@ -7,14 +7,3 @@ dotenv.config()
 createApp()
     .then(koa => koa.listen(process.env.PORT ?? 8000))
     .catch(err => console.error(err))
-
-
-import Cors from "@koa/cors"
-import BodyParser from "koa-bodyparser"
-import { DefaultFacility, PlumierApplication } from "plumier"
-
-export class WebApiFacility extends DefaultFacility {
-    async setup({ koa }: Readonly<PlumierApplication>) {
-        //do something with the Koa instance
-        koa.use(<koa middleware>)
-    }
